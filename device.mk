4afa0f2fa333d12f25527624eb667ce78c66bef2
Cristian Pardo <cristian.pardo@externos.bci.cl>
2022-01-03 17:52:11 -0300

    update

--- device.mk
+++ device.mk
@@ -14,37 +14,3 @@
 # limitations under the License.
 #
 
-PRODUCT_PACKAGES += \
-    bootctrl.xiaomi_sm8250 \
-    bootctrl.xiaomi_sm8250.recovery \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    fastbootd
-
-PRODUCT_HOST_PACKAGES += \
-    libandroidicu
-
-# SHIPPING API
-PRODUCT_SHIPPING_API_LEVEL := 29
-# VNDK API
-PRODUCT_TARGET_VNDK_VERSION := 30
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
-
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# otacert
-PRODUCT_EXTRA_RECOVERY_KEYS += \
-    $(DEVICE_PATH)/security/magisk \
-    $(DEVICE_PATH)/security/miui_releasekey
-
-# PRODUCT_RELEASE_NAME ro.twrp.device.name
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.device.name=$(PRODUCT_RELEASE_NAME)
-
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/umi/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/$(PRODUCT_RELEASE_NAME)/modules,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules) \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/$(PRODUCT_RELEASE_NAME)/firmware,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware)
